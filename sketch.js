19f2ef3e3ca10aaaf46ead58ddaddc68eb058e3f
128Archit-Singh <121339217+128Archit-Singh@users.noreply.github.com>
2023-04-28 18:30:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,16 +2,15 @@
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
-var bow , arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
-var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+var  arrow,  background, redB, pinkB, greenB ,blueB ,arrowGroup;
+var  arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
 
 var score =0;
 function preload(){
   
   backgroundImage = loadImage("background0.png");
-  
+
   arrowImage = loadImage("arrow0.png");
-  bowImage = loadImage("bow0.png");
   red_balloonImage = loadImage("red_balloon0.png");
   green_balloonImage = loadImage("green_balloon0.png");
   pink_balloonImage = loadImage("pink_balloon0.png");
@@ -28,13 +27,7 @@ function setup() {
   scene = createSprite(0,0,400,400);
   scene.addImage(backgroundImage);
   scene.scale = 2.5
-  
-  // creating bow to shoot arrow
-  bow = createSprite(380,220,20,50);
-  bow.addImage(bowImage); 
-  bow.scale = 1;
-  
-   score = 0  
+ 
  //redB= new Group();
  
  // arrowGroup= new Group();
@@ -57,8 +50,7 @@ function draw() {
        if (scene.x < 0){
           scene.x = scene.width/2;
           }
-      // //moving bow
-       bow.y = World.mouseY      
+      
    // release arrow when space key is pressed
   if (keyDown("space")) {
     createArrow();  
@@ -91,9 +83,7 @@ function draw() {
   if (gameState === END) {
     /*Uncomment correct option 
       according to END state*/  
-      
-      // //destroy bow
-      bow.destroy();
+   
       //stop background movement
       scene.velocityX = 0;
   }
@@ -162,7 +152,6 @@ function pinkBalloon() {
   var arrow= createSprite(100, 100, 60, 10);
   arrow.addImage(arrowImage);
   arrow.x = 360;
-  arrow.y=bow.y;
   arrow.velocityX = -4;
   arrow.lifetime = 100;
   arrow.scale = 0.3;
